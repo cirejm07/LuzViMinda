497369bca6e7bf9be4c2dc5b79e185d6e8a2068c
cirejm07 <90808033+cirejm07@users.noreply.github.com>
2022-07-31 09:05:58 +0800

    includes merge vue 3 cli to vite

---
+++ src/components/FooterView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer">
+    Footer
+     <fa :icon="['fab', 'facebook']" />
+     <fa :icon="['fab', 'twitter']" />
+     <fa :icon="['fab', 'github']" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterView'
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, fab)
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+createApp(App).use(router).use(VueSweetalert2).component('fa', FontAwesomeIcon).mount('#app')
+
+
+import "bootstrap/dist/js/bootstrap.js"
+
